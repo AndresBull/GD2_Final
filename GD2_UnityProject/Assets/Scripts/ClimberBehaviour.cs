25bedcf1773dfeb5c4d90e8e11879ee9095aa113
Erik De Weert <edu01.edw.wesie@gmail.com>
2020-12-03 10:41:16 +0100

    Added and set up Input Manager

---
+++ GD2_UnityProject/Assets/Scripts/ClimberBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClimberBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnPlaceLadder(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnPickupLadder(InputAction.CallbackContext context)
+    {
+
+    }
+}

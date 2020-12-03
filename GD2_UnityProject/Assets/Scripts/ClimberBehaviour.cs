3b4f80f6e6db43f54a8054b98d95bc00c9c51b2b
Erik De Weert <edu01.edw.wesie@gmail.com>
2020-12-03 20:50:55 +0100

    Updated Climber Behaviour

--- GD2_UnityProject/Assets/Scripts/ClimberBehaviour.cs
+++ GD2_UnityProject/Assets/Scripts/ClimberBehaviour.cs
@@ -7,35 +7,44 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Collider))]
 public class ClimberBehaviour : MonoBehaviour
 {
-    // ==============================----- NOTE -----==================================
-    //
-    // This script is still work in progress. It is not recommended to use to playtest.
-    //
-    // ==========================----- END OF NOTE -----===============================
+    [Header("Horizontal Movement")]
+    [Tooltip("The maximum speed at which the climber moves around.")][SerializeField]
+    private float _maxSpeed = 10f;
+    
+    [Header("Jumping")]
+    [Tooltip("WARNING: PROTOTYPING ONLY\nThis multiplier can be used during prototyping to increase the jump to reach heights quickly. Not for use in the build. Set to 1 (one) to disable.")][SerializeField]
+    private float _jumpMultiplier = 1f;
+    [Tooltip("The height of the jump (in units).")][SerializeField]
+    private float _jumpHeight = 0.5f;
+    [Tooltip("The time interval in which a jump can be called before the player touches the ground (in seconds).")][SerializeField]
+    private float _jumpDelay = 0.25f;
 
-    [Tooltip("The maximum speed at which the climber moves around.")]
-    public float MaxSpeed = 10f;
-    [Tooltip("WARNING: PROTOTYPING ONLY\nThis multiplier can be used during prototyping to increase the jump to reach heights quickly. Not for use in the build.")]
-    public float JumpMultiplier = 1f;
-    public float JumpHeight = 0.5f;
+    [Header("Physics")]
+    [Tooltip("The layers to test collision against. Default tests against everything.")][SerializeField]
+    private LayerMask CollisionMask = ~0;
 
     private Vector3 _colExtents;
     private float _horizontalMovement;
+    private float _jumpForce = 3.0f;
+    private float _jumpTimer;
 
     private Rigidbody _rb;
     private Collider _col;
-    private bool _isJumping = false, _desiredJump;
+    private bool _isJumping = false;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
         _colExtents = _col.bounds.extents;
+#if !UNITY_EDITOR
+        JumpMultiplier = 1.0f;
+#endif
     }
 
     void FixedUpdate()
     {
-        float movement = _horizontalMovement * MaxSpeed * Time.fixedDeltaTime;
+        float movement = _horizontalMovement * _maxSpeed * Time.fixedDeltaTime;
         transform.position += new Vector3(movement, 0.0f, 0.0f);
         UseGravity();
     }
@@ -49,14 +58,13 @@ public class ClimberBehaviour : MonoBehaviour
     {
         if (context.ReadValueAsButton())
         {
-            // save jump call until touching the ground
-            _desiredJump = true;
             if (!_isJumping && IsGrounded())
             {
                 _isJumping = true;
-                _desiredJump = false;
                 Jump();
+                return;
             }
+            _jumpTimer = Time.time + _jumpDelay;
         }
     }
 
@@ -72,7 +80,7 @@ public class ClimberBehaviour : MonoBehaviour
 
     private bool IsGrounded()
     {
-        Physics.BoxCast(transform.position + (0.1f * Vector3.up), _colExtents, Vector3.down, out RaycastHit hit, transform.rotation, 0.1f);
+        Physics.BoxCast(transform.position + (0.1f * Vector3.up), _colExtents, Vector3.down, out RaycastHit hit, transform.rotation, 0.1f, CollisionMask);
         return hit.collider != null && _rb.velocity.y <= 0;
     }
 
@@ -81,11 +89,10 @@ public class ClimberBehaviour : MonoBehaviour
         if (IsGrounded())
         {
             _rb.useGravity = false;
-            _rb.velocity = Vector3.ProjectOnPlane(_rb.velocity, -Physics.gravity.normalized);
-            if (_desiredJump)
+            _rb.velocity = new Vector3(_rb.velocity.x, 0.0f, 0.0f);
+            if (_jumpTimer > Time.time)
             {
                 Jump();
-                _desiredJump = false;
             }
             _isJumping = false;
             return;
@@ -95,9 +102,7 @@ public class ClimberBehaviour : MonoBehaviour
 
     private void Jump()
     {
-#if UNITY_EDITOR
-        _rb.AddForce(-Physics.gravity * JumpMultiplier * JumpHeight, ForceMode.Impulse);
-#endif
-        //_rb.AddForce(-Physics.gravity.normalized, ForceMode.Force);
+        _rb.AddForce(Vector3.up * _jumpForce * _jumpMultiplier * _jumpHeight, ForceMode.Impulse);
+        _jumpTimer = 0.0f;
     }
 }

003000d64f4e1f8666425796080733904e5070ce
Yorick Bongaerts <yorickbongaerts@telenet.be>
2020-12-04 18:43:29 +0100

    BlockDropping start
    blocks drop, overlord hand has been started

---
+++ GD2_UnityProject/Assets/Scripts/BlockDropper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BlockDropper : MonoBehaviour
+{
+    public GameObject HoldBlock;
+    private float _speed = 50.0f;
+    private float _horizontalMovement;
+    private bool _hasBlock = true;
+    private float _dropTimer;
+    private float _dropDelay;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movement = _horizontalMovement * _speed * Time.fixedDeltaTime;
+        transform.position += new Vector3(movement, 0.0f, 0.0f);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _horizontalMovement = context.ReadValue<float>();
+    }
+    public void OnDrop(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton())
+        {
+            if (_hasBlock)
+            {
+                _hasBlock = false;
+                Drop();
+                return;
+            }
+        }
+    }
+
+    private void Drop()
+    {
+        Instantiate(HoldBlock, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
---
+++ GD2_UnityProject/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    private float _speed = 100.0f;
+    private float _dropDownDelay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Drop());
+    }
+
+    // Update is called once per frame
+    
+
+    private IEnumerator Drop()
+    {
+        while (Application.isPlaying)
+        {
+            this.transform.position -= Vector3.up * _speed * Time.deltaTime;
+            yield return new WaitForSeconds(_dropDownDelay);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            _speed = 0;
+        }
+    }
+}

4253460082c00f152844243f9c0c21ecbeb9b487
luckyelias <eliaslefebre@ymail.com>
2020-12-05 12:13:34 +0100

    Added grid system
    Will still add an easy way to check what kind of blocks are in a certain grid

---
+++ GD2_UnityProject/Assets/Scripts/GridScripts/GridPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridScripts
+{
+    public struct GridPosition
+    {
+        public int X;
+        public int Y;
+    }
+}

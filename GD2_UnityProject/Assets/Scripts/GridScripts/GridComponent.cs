07f1b62a6802863ab76a440d6631bafe3bb149d7
luckyelias <eliaslefebre@ymail.com>
2020-12-05 15:49:53 +0100

    Added an easy way to find if something is colliding with a grid position

---
+++ GD2_UnityProject/Assets/Scripts/GridScripts/GridComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class GridComponent
+{
+    private static List<GameObject> GetCollidingObjects(GameObject gridComponent)
+    {
+        Vector3 gridComponentDimensions =(gridComponent.transform.localScale);
+
+        Collider[]colliders = Physics.OverlapBox(gridComponent.transform.position, gridComponentDimensions);
+        
+        List<GameObject> colliderGameObjects = new List<GameObject>();
+        foreach (Collider collider in colliders)
+        {
+            colliderGameObjects.Add(collider.gameObject);
+        }
+
+        return colliderGameObjects;
+    }
+
+    private static bool ContainsGameObjectInLayer(GameObject gridComponent, LayerMask layerMask,out GameObject collidingGameObject)
+    {
+        List<GameObject> allCollidingGameObjects = GetCollidingObjects(gridComponent);
+        foreach (GameObject gameObject in allCollidingGameObjects)
+        {
+            if (gameObject.layer == layerMask)
+            {
+                collidingGameObject = gameObject;
+                return true;
+            }
+        }
+
+        collidingGameObject = null;
+        return false;
+    }
+}

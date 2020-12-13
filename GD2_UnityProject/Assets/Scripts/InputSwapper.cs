d3a91ae31eae6e9c86124cec73408d77077f5cbf
Yorick Bongaerts <yorickbongaerts@telenet.be>
2020-12-13 14:46:41 +0100

    Start of combination
    Tried to make the multiple actionmappings work together, don't know if it works.
    Also put some of the WIP's together

---
+++ GD2_UnityProject/Assets/Scripts/InputSwapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSwapper : MonoBehaviour
+{
+    private PlayerInputManager manager;
+    public GameObject OverlordPrefab;
+    public InputActionReference OverlordReference;
+    private void Start()
+    {
+        manager = gameObject.GetComponent<PlayerInputManager>();
+    }
+    private void Update()
+    {
+        if (manager.playerCount == 3)
+        {
+            manager.playerPrefab = OverlordPrefab;
+            manager.joinAction.reference.Set(OverlordReference);
+        }
+    }
+}

cf93aeebb4ee486351990fb62bbd8e2d37563f63
Elias Lefebre <elias.lefebre@student.howest.be>
2020-12-30 17:21:05 +0100

    Added new blocks and a script to make them seem more randomized

---
+++ GD2_UnityProject/Assets/Scripts/Graphical/RandomlyShowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyShowScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _percentChanceObjectGetsShown = 20f;
+
+    void Start()
+    {
+        float randomNumber = Random.Range(1, 101);
+
+        if (!ShouldDecalGetShown(randomNumber))
+            Destroy(this.gameObject);
+    }
+
+    private bool ShouldDecalGetShown(float randomNumber)
+    {
+        return _percentChanceObjectGetsShown>=randomNumber;
+    }
+}

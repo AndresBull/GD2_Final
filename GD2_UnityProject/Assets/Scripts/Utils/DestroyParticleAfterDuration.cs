19868497581cb766b167ccc96c606f38c4d6939c
AndresBull <56974103+AndresBull@users.noreply.github.com>
2021-01-05 22:23:17 +0100

    added dust cloud for block drop

---
+++ GD2_UnityProject/Assets/Scripts/Utils/DestroyParticleAfterDuration.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleAfterDuration : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(this.gameObject, this.GetComponent<ParticleSystem>().main.duration);
+    }
+}

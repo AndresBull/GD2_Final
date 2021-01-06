6c7186199f747d4cc6771963e27c85a46587665c
Elias Lefebre <elias.lefebre@student.howest.be>
2021-01-06 23:48:14 +0100

    Sounds and stuff

---
+++ GD2_UnityProject/Assets/Scripts/SoundSystem/MusicTransitionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTransitionScript : MonoBehaviour
+{
+    private static MusicTransitionScript instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}

e487a1a30b6449540f86a4784d9cb9116f140aab
AndresBull <56974103+AndresBull@users.noreply.github.com>
2021-01-07 11:23:56 +0100

    Project settings and splach screen

---
+++ GD2_UnityProject/Assets/makeScreenshot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeScreenshot : MonoBehaviour
+{
+    private bool IsScreenSHotTake = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsScreenSHotTake)
+        {
+            Debug.Log("Screenshot");
+            ScreenCapture.CaptureScreenshot("TestScreenSHot", 2);
+
+            IsScreenSHotTake = true;
+        }
+    }
+}
